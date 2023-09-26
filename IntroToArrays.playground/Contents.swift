cfd67d866bf963ecee7c0910c8c55ecbe9636517
KO-d14 <ainajk@hawaii.edu>
2023-09-27 08:28:58 +0900

    Completed Parts 1-8
    - Stiil need to do problem set

---
+++ IntroToArrays.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+var greeting = "Hello, playground"
+//Part 3
+//Declare an array named "fruitNames" and this array is going to hold objects of type String
+
+var fruitNames: [String]
+
+//Part4
+fruitNames = ["Banana", "Strawberry", "Cherry"]
+
+//Part 5 ON YOUR OWN
+print(fruitNames)
+
+//Part 6
+//"I like to eat Banana"
+print("I like to eat Banana")
+
+//Part 7
+// It does a for-loop
+for name in fruitNames
+{
+    print("I like to eat " + name)
+}
+
+//Part 8
+print(fruitNames[1])
+
